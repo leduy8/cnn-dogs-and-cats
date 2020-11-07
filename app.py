4406a48c0907e45ebb0b245ec5dd6152c8d52f8e
duyle <duy.leduc8@gmail.com>
2020-11-07 12:25:24 +0700

    prepare images for cnn

--- app.py
+++ app.py
@@ -15,3 +15,68 @@ import glob
 import matplotlib.pyplot as plt
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# * Disable GPU for tensorflow
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+# * Using GPU for processing
+# physical_devices = tf.config.experimental.list_physical_devices("GPU")
+# #print("Num GPUs Available: ", len(physical_devices))
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# * Pickup images of dogs and cats and put it in the corresponding folders
+# os.chdir("data/dogs-vs-cats")
+# if os.path.isdir("train/dog") is False:
+#     os.makedirs("train/dog")
+#     os.makedirs("train/cat")
+#     os.makedirs("valid/dog")
+#     os.makedirs("valid/cat")
+#     os.makedirs("test/dog")
+#     os.makedirs("test/cat")
+
+#     for i in random.sample(glob.glob("cat*"), 500):
+#         shutil.move(i, 'train/cat')
+#     for i in random.sample(glob.glob("dog*"), 500):
+#         shutil.move(i, 'train/dog')
+#     for i in random.sample(glob.glob("cat*"), 100):
+#         shutil.move(i, 'valid/cat')
+#     for i in random.sample(glob.glob("dog*"), 100):
+#         shutil.move(i, 'valid/dog')
+#     for i in random.sample(glob.glob("cat*"), 50):
+#         shutil.move(i, 'test/cat')
+#     for i in random.sample(glob.glob("dog*"), 50):
+#         shutil.move(i, 'test/dog')
+# os.chdir("../../")
+
+train_path = 'data/dogs-vs-cats/train'
+valid_path = 'data/dogs-vs-cats/valid'
+test_path = 'data/dogs-vs-cats/test'
+
+# * Create batches of normalized tensor image data for train
+# ? Use preprocessing model vgg16
+# ? Target size for image resolution
+train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(
+    directory=train_path, target_size=(224, 224), classes=['cat', 'dog'], batch_size=10)
+
+valid_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(
+    directory=valid_path, target_size=(224, 224), classes=['cat', 'dog'], batch_size=10)
+
+test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(
+    directory=test_path, target_size=(224, 224), classes=['cat', 'dog'], batch_size=10, shuffle=False)
+
+# * Visualizing data
+imgs, labels = next(train_batches)
+
+
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 10, figsize=(20, 20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+plotImages(imgs)
+print(labels)
