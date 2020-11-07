46c70ffafc3b0f19346879aa8103a50adbea4ad2
duyle <duy.leduc8@gmail.com>
2020-11-07 17:10:21 +0700

    predict test dataset

--- app.py
+++ app.py
@@ -68,14 +68,14 @@ test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.v
 # imgs, labels = next(train_batches)
 
 
-# def plotImages(images_arr):
-#     fig, axes = plt.subplots(1, 10, figsize=(20, 20))
-#     axes = axes.flatten()
-#     for img, ax in zip(images_arr, axes):
-#         ax.imshow(img)
-#         ax.axis('off')
-#     plt.tight_layout()
-#     plt.show()
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 10, figsize=(20, 20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
 
 
 # print(labels)
@@ -101,3 +101,62 @@ model.compile(optimizer=Adam(learning_rate=0.0001),
 # ? steps_per_epoch = num of dataset / batch_size should be equal to 100. But we specify batch size of train_batches is 10 so it's equal to 100, same as the steps_per_epoch so we only need to use len() for more general (note that this is a coincidence, if not, then we set as specific number)
 model.fit(x=train_batches, steps_per_epoch=len(train_batches),
           validation_data=valid_batches, validation_steps=len(valid_batches), epochs=10, verbose=2)
+
+# * Extract batch of images and their corresponding labels
+test_imgs, test_labels = next(test_batches)
+
+plotImages(test_imgs)
+print(test_labels)
+
+# ? Get corresponding labels for test dataset
+print(test_batches.classes)
+
+# * Predict test dataset
+predictions = model.predict(x=test_batches, steps=len(test_batches), verbose=0)
+# ? Round predictions to see what the data look like
+np.round(predictions)
+
+# * Create a confusion matrix
+cm = confusion_matrix(y_true=test_batches.classes,
+                      y_pred=np.argmax(perdictions, axis=-1))
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+# ? Inspect the class so we pass in the correct order
+print(test_batches.class_indices)
+
+cm_plot_labels = ['cat', 'dog']
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title="Confusion Matrix")
